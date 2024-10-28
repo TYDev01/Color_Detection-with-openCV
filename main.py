f1d67f83f880f0ebd707976f433fb626d9db9169
Anthony <anthonychinaemeru84@yahoo.com>
2024-10-28 16:41:10 +0100

    Camera working perfectly fine

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import numpy as np
 import cv2 as cv
+
+cap = cv.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
