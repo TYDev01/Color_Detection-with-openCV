950fa27d79e71d12b2ab40246b324e728fd68d57
Anthony <anthonychinaemeru84@yahoo.com>
2024-10-28 17:03:41 +0100

    Correct dtype=np.unit8 to dtype=np.uint8

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ def get_limits(color):
     lowerLimit = hsvC[0][0][0] - 10, 100, 100
     upperLimit = hsvC[0][0][0] + 10, 255, 255
 
-    lowerLimit = np.array(lowerLimit, dtype=np.unit8)
-    upperLimit = np.array(upperLimit, dtype=np.unit8)
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
 
     return lowerLimit, upperLimit
